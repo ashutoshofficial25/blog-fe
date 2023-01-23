ad6652a127c3c4c377b437cc79a7ef8e8d453c20
Ashutosh Maurya <ashutoshm9695@gmail.com>
2023-01-23 20:11:27 +0530

    hope

--- src/views/BlogPage/index.js
+++ src/views/BlogPage/index.js
@@ -37,8 +37,8 @@ const BlogPage = () => {
           <img
             className="w-full"
             src={
-              blog.thumbnail
-                ? `http://localhost:3008/uploads/blogImages/${blog.thumbnail}`
+              blogDetail.thumbnail
+                ? `http://localhost:3008/uploads/blogImages/${blogDetail.thumbnail}`
                 : "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
             }
             alt="image of the blog"
