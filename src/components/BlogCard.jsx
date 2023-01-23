326cb38fb9363aca99ca0cb1464c3167189a193d
Ashutosh Maurya <dev26@bytescrum.com>
2023-01-23 18:30:03 +0530

    image issue fixed

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,25 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { getImage } from "../services/blogServices";
 
 const BlogCard = ({ blog }) => {
   console.log(blog);
-  //need of description
+  //need of 
+  
 
   return (
-    <div className="card border p-1 border-slate-500 shadow-xl w-fit rounded-xl">
+
+    // https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <Link to={`/blog/${blog._id}`} className="content ">
-        <img
-          src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
-          alt="image"
-          className="w-56 object-cover rounded-xl"
-        />
-        <div className="card-title text-xl pt-2 font-bold">{blog.title}</div>
-        <article className="px-3 py-2 w-56 font-semibold">
-          <p>{blog.description}</p>
-        </article>
+      <img className="w-full" src='http://localhost:3008/uploads/blogImages/pho.jpeg' alt={blog.title} />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{blog.title}</div>
+        <p className="text-gray-700 text-base">
+          {blog.description}
+        </p>
+      </div>
       </Link>
     </div>
+   
   );
 };
 
 export default BlogCard;
+//  <div className="card border p-1 border-slate-500 shadow-xl w-fit rounded-xl">
+//       <Link to={`/blog/${blog._id}`} className="content ">
+//         <img
+//           src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
+//           alt="image"
+//           className="w-56 object-cover rounded-xl"
+//         />
+//         <div className="card-title text-xl pt-2 font-bold">{blog.title}</div>
+//         <article className="px-3 py-2 w-56 font-semibold">
+//           <p>{blog.description}</p>
+//         </article>
+//       </Link>
+//     </div>
