d44714b922f853c10ecd7c0af81e717788fee7a8
Ashutosh Maurya <dev26@bytescrum.com>
2023-02-07 12:11:23 +0530

    logout working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,7 @@ const Navbar = () => {
 
   const handleLogout = () => {
     localStorage.removeItem("token");
+    localStorage.removeItem('account')
     dispatch(logout());
 
     navigate("/login");
